cc2465756e0dd7dd6d09fca6db82f78cf196ff44
btb0 <benburke2016@gmail.com>
2023-08-19 23:57:03 -0400

    component styling

--- src/components/AirConditions/AirConditions.jsx
+++ src/components/AirConditions/AirConditions.jsx
@@ -5,25 +5,25 @@ export default function AirConditions({ weatherData, chanceOfRain }) {
   return (
     <section className="bg-componentDark rounded-3xl flex flex-col p-6 h-1/3">
       <div className="flex justify-between">
-        <h2 className="mb-3">Air Conditions</h2>
+        <h2 className="mb-3 text-darkGreyText">Air Conditions</h2>
         <button className="rounded-full bg-blue-600">See more</button>
       </div>
       <div className="grid grid-rows-2 grid-cols-2 h-full">
         <div className="h-1/2">
-          <p>feels like</p>
-          <p>{weatherData.current.feelslike_c}&deg;</p>
+          <p className="text-darkGreyText">Real Feel</p>
+          <p className="text-greyText">{weatherData.current.feelslike_c}&deg;</p>
         </div>
         <div className="h-1/2">
-          <p>wind</p>
-          <p>{weatherData.current.wind_kph} km/h</p>
+          <p className="text-darkGreyText">Wind</p>
+          <p className="text-greyText">{weatherData.current.wind_kph} km/h</p>
         </div>
         <div className="h-1/2">
-          <p>chance of rain</p>
-          <p>{chanceOfRain()}%</p>
+          <p className="text-darkGreyText">Chance of rain</p>
+          <p className="text-greyText">{chanceOfRain()}%</p>
         </div>
         <div className="h-1/2">
-          <p>UV index: {weatherData.current.uv}</p>
-          <p>{weatherData.current.uv}</p>
+          <p className="text-darkGreyText">UV Index</p>
+          <p className="text-greyText">{weatherData.current.uv}</p>
         </div>
       </div>
     </section>
